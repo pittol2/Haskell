029d8b742cc87ecfa964d168842cf64ad3e427c3
pittol2 <38327401+pittol2@users.noreply.github.com>
2018-05-03 14:57:15 -0300

    Add files via upload

--- exercicio2-Lista2.hs
+++ exercicio2-Lista2.hs
@@ -1,8 +1,8 @@
 la x = 0 < x && x < 100 && mod x 3 == 0 && mod x 5 == 0
 
 lb a b = if a == True && b == True 
-	 then False 
-	 else a || b
+         then False 
+         else a || b
 
 lc d1 m1 a1 d2 m2 a2 = (d2 - d1) + (m2 - m2)*30 + (a2 - a1)*12*30
 
@@ -10,4 +10,24 @@ ld l1 l2 l3 = l1 + l2 > l3 && l1 + l3 > l2 && l2 + l3 > l1
 
 lei x y xe ye xd yd = x >= xe && x <= xd && y <= ye && y <= yd
 
-leii x y xe ye xs ys = x >= xe && x <= 2*xs-xe && y < 
+leii x y xe ye xs ys = abaixodareta1 x y xe ye xs ys && 
+                                           acimadareta2 x y xe ye xs ys && 
+                                           abaixodareta3 x y xe ye xs ys && 
+                                           acimadareta4 x y xe ye xs ys
+
+abaixodareta1 x y xe ye xs ys = y <= (ys - ye)/(xs - xe)*(x - xs) + ys
+
+acimadareta2 x y xe ye xs ys = y >= ((2*ye - ys) - ye)/(xs - xe)*(x - xe) + ye
+
+abaixodareta3 x y xe ye xs ys = y <= (ye - ys)/((2*xs - xe) - xs)*(x - xs) + ys
+
+acimadareta4 x y xe ye xs ys = y >= ((2*ye - ys) - ye)/(xs - (2*xs - xe))*(x - xs) + (2*ye - ys)
+
+leiii x y xc yc r = r^2 <= (x - xc)^2 + (y - yc)^2
+
+lf a b c = if a == b && b == c
+                            then 3 else
+                   if a == b && b /= c || a == c && b /= c || b == c && a /= b
+                            then 2
+                    else 1
+
